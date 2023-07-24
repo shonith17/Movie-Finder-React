d2e5c39ae37d3fe9472caa0124df496fab637d6c
SHON PURDY <SHONPURDY@GMAIL.COM>
2023-07-24 12:50:00 -0700

    take out switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link} from "react-router-dom";
 import Home from './Home';
 import Movie from './Movie';
 
@@ -15,11 +15,9 @@ const App = () => {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <Link className="navbar-brand" to="/">Movie Finder</Link>
       </nav>
-      <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/movie/:id" component={Movie} />
         <Route component={NotFound} />
-      </Switch>
     </Router>
     </>
   );
